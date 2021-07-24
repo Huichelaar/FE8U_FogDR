92abd72839ba659a1594e0fc363a183db26d4a08
Huichelaar <76848124+Huichelaar@users.noreply.github.com>
2021-07-24 12:29:16 +0200

    Two more recalcs spared.
    Don't recalc DR when unit doesn't move before committing an action.
    
    Don't recalc DR when unit cancels an action where they didn't move.

--- ASM/ActionCancelDR.asm
+++ ASM/ActionCancelDR.asm
@@ -1,12 +1,46 @@
 @ Recalc DR when cancelling a move.
-@ Hooked at 0x1D028
+@ Hooked at 0x1D014
+@   r2: 0202BE48, gActiveUnitPosition
 .thumb
 
-push  {r4, r14}
+push  {r4-r6, r14}
+mov   r4, r2
+ldr   r5, =ActiveUnit
+ldr   r5, [r5]
 
 
+ldrh  r0, [r4]
+ldrb  r1, [r5, #0x10]
+ldrh  r2, [r4, #0x2]
+ldrb  r3, [r5, #0x11]
+strb  r0, [r5, #0x10]           @ Vanilla updates
+strb  r2, [r5, #0x11]           @ these values like this.
+mov   r6, #0x0
+cmp   r0, r1
+bne   L2
+  cmp   r2, r3
+  bne   L2
+    b     L3
+L2:
+mov   r6, #0x1
+L3:
+
+
+@ Vanilla.
+mov   r0, r5
+ldr   r4, =ApplyUnitMovement
+bl    GOTO_R4
+ldr   r0, [r5, #0xC]
+mov   r1, #0x2
 bic   r0, r1
 str   r0, [r2, #0xC]
+
+
+@ Skip if unit didn't move.
+cmp   r6, #0x0
+beq   Return
+
+
 ldr   r4, =RefreshFogAndUnitMaps
 bl    GOTO_R4
 
@@ -59,7 +93,7 @@ bl    GOTO_R4
 
 
 Return:
-pop   {r4}
+pop   {r4-r6}
 pop   {r0}
 bx    r0
 GOTO_R4:
